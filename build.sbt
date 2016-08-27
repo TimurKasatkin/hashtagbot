d65d51d71e8cc095f11aa5ec97be9a5d3438c03c
Timur Kasatkin <aronwest001@gmail.com>
2016-08-27 22:55:24 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "hashtagbot"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+resolvers += "jitpack" at "https://jitpack.io"
+
+libraryDependencies += "com.github.mukel" % "telegrambot4s" % "v1.2.1"
+
+    
---
+++ src/main/scala/HashTagBot.scala
@@ -0,0 +1,31 @@
+import info.mukel.telegrambot4s.api.{Commands, TelegramBot, Webhook}
+import info.mukel.telegrambot4s.methods.AnswerInlineQuery
+import info.mukel.telegrambot4s.models._
+
+import scala.io.Source
+
+/**
+  * @author Timur Kasatkin 
+  * @date 24.08.16.
+  * @email aronwest001@gmail.com
+  * @email t.kasatkin@innopolis.ru
+  */
+object HashTagBot extends TelegramBot with Commands
+	//	with Polling
+	with Webhook {
+	override def token: String = Source.fromInputStream(getClass.getResourceAsStream("bot.token")).getLines.next
+
+	override def handleInlineQuery(inlineQuery: InlineQuery): Unit = {
+		if (inlineQuery.query.nonEmpty) {
+			val result = "#" + inlineQuery.query.replaceAllLiterally(" ", "_")
+			api.request(AnswerInlineQuery(inlineQuery.id, Seq(
+				InlineQueryResultArticle("0", result, InputTextMessageContent(result)))))
+		}
+	}
+
+	def main(args: Array[String]): Unit = this.run()
+
+	override def port: Int = 80
+
+	override def webhookUrl: String = "http://telegram-hashtag-bot.herokuapp.com/"
+}
